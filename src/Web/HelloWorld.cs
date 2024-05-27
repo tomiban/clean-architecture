4f57ad32b82ff776fb59f1324762c186039e088d
t0mib4n <tbanchio15@gmail.com>
2024-05-27 15:29:13 -0300

    feat:TB: add class test.cs
    include stastic method for print hello world

---
+++ src/Web/HelloWorld.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Web
+{
+    public static class Test
+    {
+        public static void HelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
